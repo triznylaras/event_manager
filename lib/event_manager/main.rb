4b420bfd8703b6de27ca326e646fa58af187de64
triznylaras <triznylaras@gmail.com>
2021-12-06 13:37:55 +0700

    wip refactor code

--- lib/event_manager/main.rb
+++ lib/event_manager/main.rb
@@ -4,31 +4,17 @@ require 'erb'
 
 class Main
   def initialize
-    # puts 'EventManager initialized.'
     @contents = CSV.open(
       'event_attendees.csv',
       headers: true,
       header_converters: :symbol
     )
     @content_size = CSV.read('event_attendees.csv').length
-    @template_letter = File.read('form_letter.erb')
-    @erb_template = ERB.new @template_letter
+    template_letter = File.read('form_letter.erb')
+    @erb_template = ERB.new template_letter
     @hour_of_day = Array.new(@content_size)
     @day_of_week = Array.new(@content_size)
-    cal
     process_content
-  end
-
-  def cal
-    @cal = {
-      0 => 'sunday',
-      1 => 'monday',
-      2 => 'tuesday',
-      3 => 'wednesday',
-      4 => 'thursday',
-      5 => 'friday',
-      6 => 'saturday'
-    }
   end
 
   def clean_zipcode(zipcode)
@@ -58,10 +44,10 @@ class Main
     File.open(filename, 'w') { |file| file.puts form_letter }
   end
 
-  def clean_homephone(homephone)
-    homephone.gsub!(/[^\d]/, '')
-    return homephone if homephone.length == 10
-    return homephone[1..10] if homephone.length == 11 && homephone[0] == 1
+  def clean_homephone(phone)
+    phone.gsub!(/[^\d]/, '')
+    return phone if phone.length == 10
+    return phone[1..10] if phone.length == 11 && phone[0] == 1
 
     'Bad number'
   end
@@ -70,22 +56,42 @@ class Main
     array.max_by { |a| array.count(a) }
   end
 
+  def parse_day
+    {
+      0 => 'sunday',
+      1 => 'monday',
+      2 => 'tuesday',
+      3 => 'wednesday',
+      4 => 'thursday',
+      5 => 'friday',
+      6 => 'saturday'
+    }
+  end
+
+  def parse_date(date)
+    parse_regdate = DateTime.strptime(date, '%m/%d/%y %H:%M')
+    @hour_of_day[@data_count] = parse_regdate.hour
+    @day_of_week[@data_count] = parse_regdate.wday
+  end
+
+  def content_data(row)
+    # name = row[:first_name]
+    @homephone = clean_homephone(row[:homephone])
+    zipcode = clean_zipcode(row[:zipcode])
+    @legislators = legislators_by_zipcode(zipcode)
+  end
+
+  puts 'EventManager initialized.'
+
   def process_content
     @content_size -= 1
 
-    count = 0
+    @data_count = 0
     @contents.each do |row|
       id = row[0]
-      name = row[:first_name]
-      homephone = clean_homephone(row[:homephone])
-      zipcode = clean_zipcode(row[:zipcode])
-      legislators = legislators_by_zipcode(zipcode)
-
-      reg_date = row[:regdate]
-      parse_regdate = DateTime.strptime(reg_date, '%m/%d/%y %H:%M')
-      @hour_of_day[count] = parse_regdate.hour
-      @day_of_week[count] = parse_regdate.wday
-      count += 1
+      content_data(row)
+      reg_date = parse_date(row[:regdate])
+      @data_count += 1
 
       form_letter = @erb_template.result(binding)
       save_thank_you_letter(id, form_letter)
@@ -95,6 +101,6 @@ class Main
 
   def print_content_info
     puts "Most active hour is : #{count_freq(@hour_of_day)}"
-    puts "Most active day is : #{@cal[count_freq(@day_of_week)]}"
+    puts "Most active day is : #{parse_day[count_freq(@day_of_week)]}"
   end
 end
