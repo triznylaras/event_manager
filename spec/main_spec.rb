be1749e34c736b59d2f08a04796b5423ee9a681d
triznylaras <triznylaras@gmail.com>
2021-12-07 14:29:13 +0700

    add clean_homephone spec

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -30,4 +30,31 @@ describe Main do
       expect(main.send(:legislators_by_zipcode, '')).to eq('You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials')
     end
   end
+
+  describe '#clean_homephone' do
+    it "return 'Bad number' if number's length is less than 10 digits or more than 11 digits" do
+      homephone = 9.82E+00
+      expect(main.send(:clean_homephone, homephone.to_s)).to eq('Bad number')
+    end
+
+    it 'return homephone if phone number is exactly 10 digits' do
+      homephone = 808_497_400_0
+      expect(main.send(:clean_homephone, homephone.to_s)).to eq(homephone.to_s)
+    end
+
+    it 'trim the first digit and use remaining 10 digits if phone number is 11 digits and first number is 1' do
+      homephone = 140_186_850_00
+      expect(main.send(:clean_homephone, homephone.to_s)).to eq(homephone.to_s[1..10])
+    end
+
+    it "return 'Bad number' if number is 11 digits and the first number is not 1" do
+      homephone = 280_849_740_00
+      expect(main.send(:clean_homephone, homephone.to_s)).to eq('Bad number')
+    end
+
+    it "return 'Bad number' if number is more than 11 digits" do
+      homephone = 414-520-50001
+      expect(main.send(:clean_homephone, homephone.to_s)).to eq('Bad number')
+    end
+  end
 end
